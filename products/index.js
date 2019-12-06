36973d308bd9fcbc5bf5f8be60311b7265d7ed9a
Eric <shuo.yang@bell.ca>
2019-12-05 23:15:49 -0500

    POST: Receive product.json from req body

--- products/index.js
+++ products/index.js
@@ -1,10 +1,12 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP trigger function processed a request for verifying product list.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.body && req.query) {
+        const products = req.body
+
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: products
         };
     }
     else {
