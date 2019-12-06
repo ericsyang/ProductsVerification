4cace8d8f2c43607fb79d90465088c6ff4ecf398
Eric <shuo.yang@bell.ca>
2019-12-06 00:13:25 -0500

    Test: update test to match return with expect obj

--- test/products/validation.test.js
+++ test/products/validation.test.js
@@ -5,6 +5,7 @@ const { getValidProductList, getInvalidProductList } = require('../../products/v
 
 const products = require('../testData/products.json')
 const expectValidProduct = require('../testData/expectValidProduct.json')
+const expectInvalidProduct = require('../testData/expectInvalidProduct.json')
 
 describe('getValidProductList', () => {
     test('Should get productList has a description, price is a number, and UPC is a 12 digit number', ()=>{
@@ -13,6 +14,6 @@ describe('getValidProductList', () => {
 })
 describe('getInvalidProductList', () => {
     test('Should get invalid productList does not haave a description, or price is not numeric, or UPC is not a 12 digit numeric string', ()=>{
-        expect(getInvalidProductList(products)).toEqual({'product': 'invalid'})
+        expect(getInvalidProductList(products)).toEqual(expectInvalidProduct)
     })
 })
