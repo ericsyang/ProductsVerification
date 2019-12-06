f415471379ef714899ce2a91986e95ce6fe15f44
Eric <shuo.yang@bell.ca>
2019-12-05 23:25:45 -0500

    Func: call basePath/api/products and query validProduct to get valid product list or invalid product list

--- products/index.js
+++ products/index.js
@@ -1,12 +1,15 @@
+'use strict'
+const { getValidProductList, getInvalidProductList } = require('./validation')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request for verifying product list.');
 
     if (req.body && req.query) {
         const products = req.body
 
+        const productList = req.query.validProduct !== 'true'? getInvalidProductList(products) : getValidProductList(products)
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: products
+            body: productList
         };
     }
     else {
---
+++ products/validation.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const getValidProductList = (products) => {
+    let validProductList = {'product': 'valid'}
+    return validProductList
+}
+const getInvalidProductList = (products) => {
+    let inValidProductList = {'product': 'invalid'}
+    return inValidProductList
+}
+
+module.exports = { getValidProductList, getInvalidProductList }
