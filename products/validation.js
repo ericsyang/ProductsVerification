6726d3c2899753586e580cf1cd76006586bf2042
Eric <shuo.yang@bell.ca>
2019-12-06 00:11:21 -0500

    Func: update getValidProductList to use filters to get valid product list

--- products/validation.js
+++ products/validation.js
@@ -1,7 +1,11 @@
 'use strict'
+const { hasDescription, isPriceNumeric, isUPCTwelveDigitNumber } = require('../lib/filter')
 
 const getValidProductList = (products) => {
-    let validProductList = {'product': 'valid'}
+    let validProductList = {}
+    Object.keys(products)
+        .map((key) => validProductList[key] = products[key]
+        .filter((product) => hasDescription(product) && isPriceNumeric(product) && isUPCTwelveDigitNumber(product)))
     return validProductList
 }
 const getInvalidProductList = (products) => {
